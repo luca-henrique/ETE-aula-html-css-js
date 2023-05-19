fca0656f14d71e69b21b311b9652e48bf41c105c
Lucas Henrique <lukas.paes18@gmail.com>
2023-05-19 16:02:22 -0300

    feature: salvar e carregar no localStorage

--- script.js
+++ script.js
@@ -81,3 +81,18 @@ btnSalvar.onclick = e => {
   loadItens()
   id = undefined
 }
+
+
+function loadItens() {
+  itens = getItensBD()
+  tbody.innerHTML = ''
+  itens.forEach((item, index) => {
+    insertItem(item, index)
+  })
+
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
