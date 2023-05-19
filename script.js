de966a0665dfa3b0a4a032f9963645dbab070e91
Lucas Henrique <lukas.paes18@gmail.com>
2023-05-19 17:36:40 -0300

    fix: ajustando eventos

--- script.js
+++ script.js
@@ -1,49 +1,60 @@
-const modal = document.querySelector('.modal-container')
-const tbody = document.querySelector('tbody')
-const sNome = document.querySelector('#m-nome')
-const sFuncao = document.querySelector('#m-funcao')
-const sSalario = document.querySelector('#m-salario')
-const btnSalvar = document.querySelector('#btn-button-salvar')
+const modal = document.querySelector('.modal-container');
+const tbody = document.querySelector('tbody');
+const sNome = document.querySelector('#m-nome');
+const sFuncao = document.querySelector('#m-funcao');
+const sSalario = document.querySelector('#m-salario');
+const btnSalvar = document.querySelector('#btn-button-salvar');
 
-let itens
-let id
+const title = document.querySelector('#title');
 
+/*
+
+  - Exemplo
+
+*/
+
+/*
+function changerColor() {
+  title.style.color = '#ff0000';
+}
+*/
+
+let itens;
+let id;
 
 function openModal(edit = false, index = 0) {
-  modal.classList.add('active')
+  modal.classList.add('active');
 
-  modal.onclick = e => {
+  modal.onclick = (e) => {
     if (e.target.className.indexOf('modal-container') !== -1) {
-      modal.classList.remove('active')
+      modal.classList.remove('active');
     }
-  }
+  };
 
   if (edit) {
-    sNome.value = itens[index].nome
-    sFuncao.value = itens[index].funcao
-    sSalario.value = itens[index].salario
-    id = index
+    sNome.value = itens[index].nome;
+    sFuncao.value = itens[index].funcao;
+    sSalario.value = itens[index].salario;
+    id = index;
   } else {
-    sNome.value = ''
-    sFuncao.value = ''
-    sSalario.value = ''
+    sNome.value = '';
+    sFuncao.value = '';
+    sSalario.value = '';
   }
-  
 }
 
 function editItem(index) {
-
-  openModal(true, index)
+  openModal(true, index);
 }
 
 function deleteItem(index) {
-  itens.splice(index, 1)
-  setItensBD()
-  loadItens()
+  itens.splice(index, 1);
+  setItensBD();
+  loadItens();
 }
 
 function insertItem(item, index) {
-  let tr = document.createElement('tr')
+  let tr = document.createElement('tr');
 
   tr.innerHTML = `
     <td>${item.nome}</td>
@@ -55,44 +66,45 @@ function insertItem(item, index) {
     <td class="acao">
       <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
     </td>
-  `
-  tbody.appendChild(tr)
+  `;
+  tbody.appendChild(tr);
 }
 
-
-btnSalvar.onclick = e => {
+btnSalvar.onclick = (e) => {
   e.preventDefault();
-  
+
   if (sNome.value == '' || sFuncao.value == '' || sSalario.value == '') {
-    return
+    return;
   }
 
   if (id !== undefined) {
-    itens[id].nome = sNome.value
-    itens[id].funcao = sFuncao.value
-    itens[id].salario = sSalario.value
+    itens[id].nome = sNome.value;
+    itens[id].funcao = sFuncao.value;
+    itens[id].salario = sSalario.value;
   } else {
-    itens.push({'nome': sNome.value, 'funcao': sFuncao.value, 'salario': sSalario.value})
+    itens.push({
+      nome: sNome.value,
+      funcao: sFuncao.value,
+      salario: sSalario.value,
+    });
   }
 
-  setItensBD()
+  setItensBD();
 
-  modal.classList.remove('active')
-  loadItens()
-  id = undefined
-}
-
+  modal.classList.remove('active');
+  loadItens();
+  id = undefined;
+};
 
 function loadItens() {
-  itens = getItensBD()
-  tbody.innerHTML = ''
+  itens = getItensBD();
+  tbody.innerHTML = '';
   itens.forEach((item, index) => {
-    insertItem(item, index)
-  })
-
+    insertItem(item, index);
+  });
 }
 
-const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
-const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? [];
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens));
 
-loadItens()
+loadItens();
